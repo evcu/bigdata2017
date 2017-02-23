671e45216859cd2b403e81d7fb5661e85838404d
evcu <evcu@users.noreply.github.com>
2017-02-23 16:16:19 -0500

    bug fixed

--- hw1/task1/map.py
+++ hw1/task1/map.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import sys
+import csv 
 
 for entry in  csv.reader(sys.stdin, quotechar='"', delimiter=',',
                quoting=csv.QUOTE_ALL, skipinitialspace=True):
