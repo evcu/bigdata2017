ca83e85a6a8b172230dd768ea6b39609a3570fbd
evcu <evcu@users.noreply.github.com>
2017-02-23 14:41:44 -0500

    hw1

---
+++ hw1/task1/map.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import os
+
+for l in sys.stdin:
+	entry = line.strip().split(",")
+	inp_file = os.environ.get(mapreduce_map_input_file)
+	if 'parking' in inp_file:
+		print '%s\t%s,%s,%s,%s' % (entry[0],entry[14],entry[6],entry[2],entry[1])
+	elif 'open' in inp_file:
+		print '%s\t%s' % (entry[0],'open')
+	else:
+		raise 'Error'
---
+++ hw1/task1/reduce.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+flag = False
+for line in sys.stdin:
+	key, value = line.strip().split('\t',1)
+	if value and flag:
+		print line.strip()
+		flag = False
+	elif not value:
+		flag = True
