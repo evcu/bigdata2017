67bdc8fc6e59ae69591c58ad3ff955e478d5d0a4
evcu <evcu@users.noreply.github.com>
2017-02-23 15:24:34 -0500

    hw1

--- hw1/task1/map.py
+++ hw1/task1/map.py
@@ -4,7 +4,7 @@ import sys
 
 for line in sys.stdin:
 	entry = line.strip().split(",")
-	inp_file = os.environ.get(mapreduce_map_input_file)
+	inp_file = os.environ.get('mapreduce_map_input_file')
 	if 'parking' in inp_file:
 		print '%s\t%s, %s, %s, %s' % (entry[0],entry[14],entry[6],entry[2],entry[1])
 	elif 'open' in inp_file:
