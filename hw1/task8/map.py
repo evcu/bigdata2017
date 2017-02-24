1ebad84c018da763db3ae215ada4f4b57eec3600
evcu <evcu@users.noreply.github.com>
2017-02-24 16:02:13 -0500

    fixes

--- hw1/task8/map.py
+++ hw1/task8/map.py
@@ -6,7 +6,7 @@ import csv
 
 for entry in  csv.reader(sys.stdin, quotechar='"', delimiter=',',
                quoting=csv.QUOTE_ALL, skipinitialspace=True):
-	for w in entry
+	for w in entry:
 		if w!='':
 			print '%s\t1' % w
 		else:
