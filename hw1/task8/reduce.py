dfd008281326232d934c9f9ea2139b0d02cbae35
Utku Evci <ue225@login-1-1.local>
2017-03-04 17:02:18 -0500

    mod

--- hw1/task8/reduce.py
+++ hw1/task8/reduce.py
@@ -3,14 +3,18 @@ import sys
 
 c_key = float('-inf')
 c_sum = 0
+all_sum = 0
 for line in sys.stdin:
 	key, value = line.strip().split('\t',1)
 	value = int(value)
 	if key == c_key:
 		c_sum += value
+		all_sum += value
 	else:
 		if c_key != float('-inf'):
 			print '%s\t%d' %(c_key,c_sum)
 		c_key = key
 		c_sum = value
+		all_sum += value
 print '%s\t%d' %(c_key,c_sum)
+print 'ALLALL\t%d' % all_sum
