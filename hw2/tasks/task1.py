119ebec170d0ad66a078cfe250f7cb52bb5aa0f2
evcu <evcu@users.noreply.github.com>
2017-03-04 18:17:59 -0500

    hw2

--- hw2/tasks/task1.py
+++ hw2/tasks/task1.py
@@ -1,14 +1,20 @@
-#!/usr/bin/python
-import os
-import sys
-import csv 
+from __future__ import print_function
 
-for entry in  csv.reader(sys.stdin, quotechar='"', delimiter=',',
-               quoting=csv.QUOTE_ALL, skipinitialspace=True):
-	inp_file = os.environ.get('mapreduce_map_input_file')
-	if 'parking' in inp_file:
-		print '%s\t%s, %s, %s, %s' % (entry[0],entry[14],entry[6],entry[2],entry[1])
-	elif 'open' in inp_file:
-		print '%s\t%s' % (entry[0],'o')
-	else:
-		raise 'CUSTOMMMMMMMError'
+import sys
+from operator import add
+from pyspark import SparkContext
+from csv import reader
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: wordcount <file>", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext()
+    file1 = sc.textFile(sys.argv[1], 1).mapPartitions(lambda x: reader(x))
+    file2 = sc.textFile(sys.argv[2], 1).mapPartitions(lambda x: reader(x))
+
+    vials = file1.map(lambda entry: (entry[0],'%s, %s, %s, %s' % (entry[14],entry[6],entry[2],entry[1])))
+    opens = file2.map(lambda entry: (entry[0],'NotPaid'))
+    vials.subtractByKey(opens).saveAsTextFile("task1.out")
+
+    sc.stop()
