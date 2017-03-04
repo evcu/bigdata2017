bbaef78efd5918de151205b0d19a369027709a08
evcu <evcu@users.noreply.github.com>
2017-03-04 18:47:33 -0500

    hw2

--- hw2/tasks/task3.py
+++ hw2/tasks/task3.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     file1 = sc.textFile(sys.argv[1], 1).mapPartitions(lambda x: reader(x))
 
     opens = file1.map(lambda entry: (entry[2],float(entry[12])))
-    res = opens.groupByKey().map(lambda (x,v): '%s\t%.2f, %.2f' % (sum(v),sum(v)/len(v)))
+    res = opens.groupByKey().map(lambda (x,v): '%s\t%.2f, %.2f' % (x,sum(v),sum(v)/len(v)))
     res.saveAsTextFile("task3.out")
     sc.stop()
