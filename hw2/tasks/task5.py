c183ef0736807a3d5343006d8a197ace9d0698d5
evcu <evcu@users.noreply.github.com>
2017-03-04 19:29:48 -0500

    hw2

--- hw2/tasks/task5.py
+++ hw2/tasks/task5.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
 
     vials = file1.map(mapper)
 
-    res = vials.reduceByKey(add).sortBy(lambda x: x[1],False).map(lambda x: '%s\t%d'% x).first()
+    res = vials.reduceByKey(add).sortBy(lambda x: x[1],False).map(lambda x: '%s\t%d'% x).zipWithIndex().filter(lambda x: x[1]==0).map(lambda x: x[0])
     sc.parallelize(res).saveAsTextFile("task5.out")
     sc.stop()
--- hw2/tasks/task7.py
+++ hw2/tasks/task7.py
@@ -14,16 +14,14 @@ if __name__ == "__main__":
     sc = SparkContext()
     file1 = sc.textFile(sys.argv[1], 1).mapPartitions(lambda x: reader(x))
 
-    def mapper1(e):
-        if int(e[1].split('-')[-1]) in  weekend_days:
-            return (e[2] , 1.0)
+    def f1(e):
+        return int(e[1].split('-')[-1]) in  weekend_days
 
-    def mapper2(e):
-        if int(e[1].split('-')[-1]) not in  weekend_days:
-            return (e[2] , 1.0)
+    def f2(e):
+        return int(e[1].split('-')[-1]) not in  weekend_days
 
-    we = file1.map(mapper1).groupByKey().map(lambda (x,v): (x,len(v)/8.0))
-    wd = file1.map(mapper2).groupByKey().map(lambda (x,v): (x,len(v)/23.0))
+    we = file1.filter(f1).map(lambda e:(e[2] , 1.0)).groupByKey().map(lambda (x,v): (x,len(v)/8.0))
+    wd = file1.filter(f2).map(lambda e:(e[2] , 1.0)).groupByKey().map(lambda (x,v): (x,len(v)/23.0))
     res = we.union(wd).map(lambda k,v:'%s\t%.2f, %.2f' % (v[0],v[1]))
     res.saveAsTextFile("task7.out")
     sc.stop()
